902a525a8c49c85c9552a3d750bbc2c9df44c898
Dakaru <ryanalgie@gmail.com>
2015-05-04 21:05:07 +0000

    First Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def home
+    #home page
+    
+  end
+  
+  def about
+    #about page
+  end
+
+  def contact
+    #contact us form
+  end
+  
+end
