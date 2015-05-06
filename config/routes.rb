8e2729b3bc3a333e7f35a4e469547899daf19592
Ryan R Algie <ryanalgie@gmail.com>
2015-05-06 12:41:48 -0700

    commit from rubymine

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ root 'pages#home'
   get "about" => 'pages#about'
   get "contact" => 'pages#contact'
   get 'logout' => 'pages#logout'
-
+  get 'pins' => 'pins#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
